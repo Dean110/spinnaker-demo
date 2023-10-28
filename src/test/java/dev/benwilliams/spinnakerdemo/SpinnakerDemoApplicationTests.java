6a5ccb6c999893c8a6f5a5cfd283b1d2dcdd4f64
Ben Williams <DeanPBR@gmail.com>
2023-10-28 16:42:15 -0400

    Spinnaker Demo

---
+++ src/test/java/dev/benwilliams/spinnakerdemo/SpinnakerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.benwilliams.spinnakerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpinnakerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
